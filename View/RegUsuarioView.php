4b992a5b81da08741a43e1a79015b8e99244532f
AlejoGS290712 <alejandrogiraldo290712@correo.itm.edu.co>
2022-05-09 14:07:51 -0500

    update

--- View/RegUsuarioView.php
+++ View/RegUsuarioView.php
@@ -4,22 +4,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">   
-    <link rel="stylesheet" href="../CSS/RegUsuarioStyle.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../CSS/RegLogUsuarioStyle.css">
 
     <title>Registro</title>
 </head>
 <body>    
-    <form class="form-register" action="" method="post">
-        <h4>Registro</h4>
-        
-        <input class="infoUsuario" type="text" name="txtNombre" id="idNombre" placeholder="Ingrese un Nombre">
-        <input class="infoUsuario" type="text" name="txtApellido" id="idApellido" placeholder="Ingrese un Apellido">
-        <input class="infoUsuario" type="email" name="txtCorreo" id="idCorreo" placeholder="Ingrese un Correo">
-        <input class="infoUsuario" type="password" name="txtContrasenia" id="idContrasenia" placeholder="Ingrese una Contraseña">
-        
-        <input class="botons" type="submit" value="Registrar">            
+    <div class="container">   
+        <form class="form-register" action="" method="post">
+            <div class="row">
+                <h4>Registro</h4>
+            </div>
 
-        <p><a href="#"> Ya tengo Cuenta </a></p>
-    </form>
+            <div class="row">
+                <input class="infoUsuario" type="text" name="txtNombre" id="idNombre" placeholder="Ingrese un Nombre">
+            </div>
+            <div class="row">
+                <input class="infoUsuario" type="text" name="txtApellido" id="idApellido" placeholder="Ingrese un Apellido">
+            </div>
+            <div class="row">
+                <input class="infoUsuario" type="email" name="txtCorreo" id="idCorreo" placeholder="Ingrese un Correo">
+            </div>
+            <div class="row">
+                <input class="infoUsuario" type="password" name="txtContrasenia" id="idContrasenia" placeholder="Ingrese una Contraseña">
+            </div>
+            <div class="row">
+                <input class="botons" type="submit" value="Registrar">            
+            </div>
+            
+            <div class="row">
+                <p><a href="./LogUsuarioView.php"> Ya tengo Cuenta </a></p>
+            </div>
+        </form>
+    </div>    
 </body>
 </html>
