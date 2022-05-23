51f956448d6128ceea63d8d1d9454fd666113e28
pabloflorez <66337036+pabloflorez@users.noreply.github.com>
2022-05-22 20:39:52 -0500

    Update Categoria.php

--- Model/Categoria.php
+++ Model/Categoria.php
@@ -1,1 +1,60 @@
-<php
+<?php 
+
+class Categoria{
+
+    //Atributos
+    private $idCategoria;
+    private $descripcionCat;
+
+    //Metodos
+    public function __construct($idCategoria, $descripcionCat)
+    {
+        $this->idCategoria = $idCategoria;
+        $this->descripcionCat = $descripcionCat;
+    }
+
+    //Setters and Getters
+    
+    /**
+     * Get the value of idCategoria
+     */ 
+    public function getIdCategoria()
+    {
+        return $this->idCategoria;
+    }
+
+    /**
+     * Get the value of descripcionCat
+     */ 
+    public function getDescripcionCat()
+    {
+        return $this->descripcionCat;
+    }
+
+     /**
+     * Set the value of idCategoria
+     *
+     * @return  self
+     */ 
+    public function setIdCategoria($idCategoria)
+    {
+        $this->idCategoria = $idCategoria;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of descripcionCat
+     *
+     * @return  self
+     */ 
+    public function setDescripcionCat($descripcionCat)
+    {
+        $this->descripcionCat = $descripcionCat;
+
+        return $this;
+    }
+
+}
+
+?>
