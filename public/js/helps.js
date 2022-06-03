34d0d858c220acd40bb1dade65ae996c5e628052
Alejandro Giraldo Serna <104726769+AlejoGS290712@users.noreply.github.com>
2022-06-03 16:14:16 -0500

    Update helps.js

--- public/js/helps.js
+++ public/js/helps.js
@@ -18,10 +18,16 @@ function httpRequest(urlCtr, callback){
 //resive como parametros el Select y las opciones (de tipo: value1=text1,value2=text2,value3=text3,...) que se va a ingresar
 function llenarSelect(select,opcs){
     
-    for (let i = select.options.length; i >= 0; i--) {
-        select.remove(i);
-    }
+    //Se limpia el Select
+    limpiarSelect(select);
 
+    //Se crea una opcion por defecto 
+    const option = document.createElement('option');
+    option.value = "";
+    option.text = "--Seleccione una opcion--";
+    select.appendChild(option);
+
+    //Se pasa la informacion a una array
     listaOpc = opcs.split(':');
 
     if (listaOpc[0] == "Error") {
@@ -29,6 +35,7 @@ function llenarSelect(select,opcs){
         return;
     }
 
+    //Se ingresa la informacion al select
     listaOpc.forEach(opc => {
         
         const option = document.createElement('option');
@@ -42,6 +49,12 @@ function llenarSelect(select,opcs){
 
     });
    
+}
+
+function limpiarSelect(select){
+    for (let i = select.options.length; i >= 0; i--) {
+        select.remove(i);
+    }
 }
 
 //Esta funcion quita el html de una cadena de texto
